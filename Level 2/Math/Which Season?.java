e8ba48ebe87bfa489c816231a102f9c8fd9e40e5
Meghanasveeramallu <86618236+Meghanasveeramallu@users.noreply.github.com>
2022-08-31 21:46:52 +0530

    Create Which Season?.java

---
+++ Level 2/Math/Which Season?.java
@@ -0,0 +1,7 @@
+string Solution::solve(int A) {
+    if (A>=3 && A<=5) return "Spring";
+    else if (A>=6 && A<=8) return "Summer";
+    else if (A>=9 && A<=11) return "Autumn";
+    else if (A==12 || A==1 || A==2) return "Winter";
+    else return "Invalid";
+}
