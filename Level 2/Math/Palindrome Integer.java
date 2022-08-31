5cbe59a80d6f1470c66cd126b016237002e5e587
Meghanasveeramallu <86618236+Meghanasveeramallu@users.noreply.github.com>
2022-08-31 21:37:08 +0530

    Create Palindrome Integer.java

---
+++ Level 2/Math/Palindrome Integer.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int isPalindrome(int A) {
+        int temp = A;
+        int ans = 0;
+        while(temp > 0){
+            ans = temp%10 + ans*10;
+            temp /= 10;
+        }
+        if( ans == A) return 1;
+        return 0;
+    }
+}
