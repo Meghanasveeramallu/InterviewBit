46776f8548268051c587ee24354e65458306a556
Meghanasveeramallu <86618236+Meghanasveeramallu@users.noreply.github.com>
2022-10-05 11:47:21 +0530

    Create Excel Column Number.java

---
+++ Level 2/Math/Excel Column Number.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int titleToNumber(String A) {
+        int result = 0;
+        for(int i=0;i<A.length();i++){
+            char c = A.charAt(i);
+            result = result*26 + (c-'A'+1);
+        }
+        return result;
+    }
+}
